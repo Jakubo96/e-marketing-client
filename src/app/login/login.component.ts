71dc3d7389caf52d557c74ea0e961918d300cdd4
Jakub Woźny <jakubovip@gmail.com>
2020-04-26 11:56:10 +0200

    Add login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,41 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page/page';
-import * as firebase from 'nativescript-plugin-firebase';
-import { Message } from 'nativescript-plugin-firebase';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ns-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  private readonly nameControl = new FormControl('', Validators.required);
+
   constructor(private readonly page: Page) {
     this.page.actionBarHidden = true;
-  }
-
-  public ngOnInit(): void {
-    firebase
-      .init({
-        onMessageReceivedCallback: (message: Message) => {
-          // eslint-disable-next-line no-console
-          console.log(`Title: ${message.title}`);
-          // eslint-disable-next-line no-console
-          console.log(`Body: ${message.body}`);
-        },
-        onPushTokenReceivedCallback(token) {
-          // eslint-disable-next-line no-console
-          console.log('Firebase push token: ' + token);
-        },
-      })
-      .then(
-        () => {
-          // eslint-disable-next-line no-console
-          console.log('firebase.init done');
-        },
-        (error) => {
-          // eslint-disable-next-line no-console
-          console.log('firebase.init error: ' + error);
-        }
-      );
   }
 }
