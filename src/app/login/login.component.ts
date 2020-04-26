17490f9689c673e4145ec0503a1d05a3c677521a
Jakub Woźny <jakubovip@gmail.com>
2020-04-26 13:20:49 +0200

    Retrieve push token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page/page';
 import { FormControl, Validators } from '@angular/forms';
 import {
+  getString,
   hasKey,
   setString,
-  getString,
 } from 'tns-core-modules/application-settings';
 import { RouterExtensions } from 'nativescript-angular/router';
+import * as firebase from 'nativescript-plugin-firebase';
+import { isAndroid } from 'tns-core-modules/platform';
 
 @Component({
   selector: 'ns-login',
@@ -16,12 +18,14 @@ import { RouterExtensions } from 'nativescript-angular/router';
 export class LoginComponent implements OnInit {
   private readonly nameControl = new FormControl('', Validators.required);
   private readonly USER_NAME_KEY = 'username';
+  private pushToken: string;
 
   constructor(page: Page, private readonly router: RouterExtensions) {
     page.actionBarHidden = true;
   }
 
   public ngOnInit(): void {
+    this.getFirebasePushToken();
     this.checkIfIsLoggedIn();
   }
 
@@ -34,9 +38,28 @@ export class LoginComponent implements OnInit {
   private checkIfIsLoggedIn(): void {
     if (hasKey(this.USER_NAME_KEY)) {
       const username = getString(this.USER_NAME_KEY);
+      this.sendUserData(username);
       this.router.navigate(['/beacon'], { queryParams: { username } });
     }
   }
 
-  private sendUserData(username: string) {}
+  private sendUserData(username: string): void {
+    if (!isAndroid) {
+      return;
+    }
+    const macAddress = this.getMacAddress();
+    console.log(macAddress);
+  }
+
+  private getFirebasePushToken(): void {
+    firebase.addOnPushTokenReceivedCallback((token) => {
+      this.pushToken = token;
+      // eslint-disable-next-line no-console
+      console.log(token);
+    });
+  }
+
+  private getMacAddress(): string {
+    return '';
+  }
 }
